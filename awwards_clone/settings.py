e94e53b4c88e87b49aca3a95ed9dd1da2eedc82e
Lemmy Mwaura <lemminton@gmail.com>
2022-04-08 13:52:48 +0300

    setup templates

--- awwards_clone/settings.py
+++ awwards_clone/settings.py
@@ -63,7 +63,9 @@ ROOT_URLCONF = 'awwards_clone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
