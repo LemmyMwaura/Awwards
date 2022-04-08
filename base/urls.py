2dbbe97afffe38f6c03cda73e4ccc8573968fcaf
Lemmy Mwaura <lemminton@gmail.com>
2022-04-08 22:51:55 +0300

    implemented user login and registration

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+
+    path('login/', views.login_page, name='login'),
+    path('register/', views.register_user, name='register'),
 ]
--- base/views.py
+++ base/views.py
@@ -19,6 +19,57 @@ def home(request):
     context = {'projects': projects }
     return render(request, 'base/home.html', context)
 
+def login_page(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        messages.success(request, 'Already Logged in')
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except Exception as e:
+            print(e)
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or Password does not exist.')
+
+    context={ 'page':page }
+    return render(request, 'base/login_register.html', context)
+
+@login_required(login_url='login')
+def logout_user(request):
+    logout(request)
+    return redirect('home')
+
+def register_user(request):
+    page = 'register'
+    form = UserCreationForm
+
+    try:
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                user = form.save(commit=False)
+                user.username = user.username.lower()
+                user.save()
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, 'An error occured during registration')
+    except Exception as e:
+        print(e)
+        messages.error('Something went wrong. Probably a connection issue. Try again')
+
+    context = { 'page':page, 'form':form }
+    return render(request, 'base/login_register.html', context)
 
 def profile(request):
     context = {}
